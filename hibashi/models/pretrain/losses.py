3b8c00889386b5152fe61559164e95e397efe497
Elias Vansteenkiste <Elias.Vansteenkiste@gmail.com>
2020-05-14 16:57:34 +0200

    add (task1): confusion matrix

--- hibashi/models/pretrain/losses.py
+++ hibashi/models/pretrain/losses.py
@@ -1,5 +1,7 @@
+import numpy as np
 import torch
 from torch.nn import functional as tnnf
+from sklearn.metrics import confusion_matrix
 
 from hibashi.losses.loss import Loss
 
@@ -20,3 +22,30 @@ class Accuracy(Loss):
         batch_size = target.size(0)
         values, indices = pred.max(1)
         return torch.sum(indices == target).float()/batch_size
+
+
+class ConfusionMatrix(Loss):
+    def __init__(self, cls_idx_2_label):
+        super(ConfusionMatrix, self).__init__()
+        self.cls_idx_2_label = cls_idx_2_label
+        self.labels = [label for cls_idx, label in sorted(cls_idx_2_label.items(), key=lambda x: x[0])]
+
+    def __call__(self, pred: torch.Tensor, target: torch.Tensor) -> np.ndarray:
+        """
+        Calculates the elements of a confusion matrix according to the predicted and target values.
+        :param pred: network prediction
+        :param target: correspondent target
+        :return: confusion matrix array values
+        """
+        target = target.cpu().data.numpy()
+        pred = tnnf.softmax(pred, dim=1).cpu().data.numpy()
+
+        predictions = []
+        targets = []
+        for batch_idx in range(len(target)):
+            predictions.append(self.cls_idx_2_label[np.argwhere(np.max(pred[batch_idx]) == pred[batch_idx])[0, 0]])
+            targets.append(self.cls_idx_2_label[target[batch_idx]])
+
+        cm = confusion_matrix(targets, predictions, labels=self.labels)
+        return cm
+
---
+++ tests/hibashi/models/pretrain/test_losses.py
@@ -0,0 +1,57 @@
+import numpy as np
+import torch
+
+from hibashi.models.pretrain.losses import ConfusionMatrix
+
+
+class TestConfusionMatrix:
+    """
+    Some basic sanity tests
+    """
+    def test_basic(self):
+
+        cls_idx_2_article_type = {19: "Jeans",
+                                  18: "Perfume and Body Mist",
+                                  17: "Formal Shoes",
+                                  16: "Socks",
+                                  15: "Backpacks",
+                                  14: "Belts",
+                                  13: "Briefs",
+                                  12: "Sandals",
+                                  11: "Flip Flops",
+                                  10: "Wallets",
+                                  9: "Sunglasses",
+                                  8: "Heels",
+                                  7: "Handbags",
+                                  6: "Tops",
+                                  5: "Kurtas",
+                                  4: "Sports Shoes",
+                                  3: "Watches",
+                                  2: "Casual Shoes",
+                                  1: "Shirts",
+                                  0: "Tshirts"}
+        cm = ConfusionMatrix(cls_idx_2_article_type)
+
+        pred = torch.Tensor([[0., 0., .01, .99],
+                             [0., 0., 0., 1.],
+                             [0., 0., 20., 80.],
+                             [0., 0., 100., 1.]])
+
+        target = torch.Tensor([3, 3, 0, 0])
+
+        result = cm(pred, target)
+
+        expected = np.zeros((20, 20))
+        expected[3, 3] += 2
+        expected[0, 3] += 1
+        expected[0, 2] += 1
+
+        assert (result == expected).all()
+
+
+
+
+
+
+
+
