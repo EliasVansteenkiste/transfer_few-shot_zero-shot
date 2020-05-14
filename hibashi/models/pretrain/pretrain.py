ef830673ca0fec19cad6674aacbdb4dc0762e9ef
Elias Vansteenkiste <Elias.Vansteenkiste@gmail.com>
2020-05-14 17:04:57 +0200

    add (task1): confusion matrix calculation in the validation step

--- hibashi/models/pretrain/pretrain.py
+++ hibashi/models/pretrain/pretrain.py
@@ -4,8 +4,9 @@ from torch.optim import Adam
 from hibashi.framework.factory import factory
 from hibashi.losses.loss import Loss
 from hibashi.models import Model
-from hibashi.models.pretrain.losses import CE, Accuracy
+from hibashi.models.pretrain.losses import CE, Accuracy, ConfusionMatrix
 from hibashi.models.pretrain.networks.classification import ImageClassifier
+from hibashi.models.pretrain.utils import plot_confusion_matrix_validation
 
 
 class PreTrain(Model):
@@ -19,9 +20,6 @@ class PreTrain(Model):
         """
         Model.__init__(self, gpu_ids, is_train=is_train)
 
-        self.criterion_cls = CE()
-        self.criterion_acc = Accuracy()
-
         self.is_train = is_train
 
         self.net_classifier = self.define_net()
@@ -33,6 +31,32 @@ class PreTrain(Model):
 
         self.loss_cls = None
         self.loss_accuracy = None
+        self.non_scalar_metric_cm = None
+
+        self.cls_idx_2_article_type = {19: "Jeans",
+                                       18: "Perfume and Body Mist",
+                                       17: "Formal Shoes",
+                                       16: "Socks",
+                                       15: "Backpacks",
+                                       14: "Belts",
+                                       13: "Briefs",
+                                       12: "Sandals",
+                                       11: "Flip Flops",
+                                       10: "Wallets",
+                                       9: "Sunglasses",
+                                       8: "Heels",
+                                       7: "Handbags",
+                                       6: "Tops",
+                                       5: "Kurtas",
+                                       4: "Sports Shoes",
+                                       3: "Watches",
+                                       2: "Casual Shoes",
+                                       1: "Shirts",
+                                       0: "Tshirts"}
+
+        self.criterion_cls = CE()
+        self.criterion_acc = Accuracy()
+        self.criterion_cm = ConfusionMatrix(self.cls_idx_2_article_type)
 
         if self.is_train:
             self.optimizer = Adam(self.net_classifier.parameters(), lr=0.0002, betas=(0., 0.9), weight_decay=0, eps=1e-8)
@@ -42,7 +66,7 @@ class PreTrain(Model):
 
     @property
     def name_main_metric(self):
-        return "loss_cls"
+        return "loss_accuracy"
 
     def main_metric(self, metrics: dict):
         """
@@ -77,6 +101,7 @@ class PreTrain(Model):
         """Calculate metrics which are used during evaluation. This should also include all the losses"""
         self.loss_cls = self.criterion_cls(self.out_pred_cls, self.in_cls_targets)
         self.loss_accuracy = self.criterion_acc(self.out_pred_cls, self.in_cls_targets)
+        self.non_scalar_metric_cm = self.criterion_cm(self.out_pred_cls, self.in_cls_targets)
 
     def backward(self):
         """Calculate losses, gradients, and update network weights; called in every training iteration"""
@@ -89,6 +114,21 @@ class PreTrain(Model):
         self.optimizer.zero_grad()
         self.backward()
         self.optimizer.step()
+
+    def get_validation_figures(self, state: dict) -> dict:
+        """
+        Creates a dictionary containing all the figures for validation.
+        :param state: (engine object) that contains the current state items
+        :return: out_dict: with key/value : name/matplotlib figure objects
+        """
+        out_dict = {}
+        for name, metric in state.metrics.items():
+            if name == 'non_scalar_metric_cm':
+                fig = plot_confusion_matrix_validation(metric, self.criterion_cm.labels)
+                out_dict['cm'] = fig
+            else:
+                continue
+        return out_dict
 
     @property
     def visuals(self) -> dict:
