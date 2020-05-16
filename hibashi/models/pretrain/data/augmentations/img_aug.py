16236cfeaebc56c2e85fed068736ac1e3a3efbc2
Elias Vansteenkiste <Elias.Vansteenkiste@gmail.com>
2020-05-16 16:39:39 +0200

    add (task1): defs and tests for FlipLR and RandomResizedCropFlip augmentations

--- hibashi/models/pretrain/data/augmentations/img_aug.py
+++ hibashi/models/pretrain/data/augmentations/img_aug.py
@@ -1,6 +1,7 @@
 """
 This module contains the image augmentation classes for the age model
 """
+import imgaug
 from imgaug import augmenters as iaa
 
 from hibashi.data.img_aug import AugmentImage
@@ -19,6 +20,45 @@ class PadToSquareResize(AugmentImage):
             #  since we have some border replicate artifacts
             iaa.size.PadToSquare(pad_mode='edge'),
             iaa.Resize({"height": 128, "width": 128})
+        ])
+
+    def augment_img(self, image):
+        """
+        Apply augmentations
+        :param image: The image that needs to be augmented
+        :return: the augmented image
+        """
+        return self.sequence.augment_image(image)
+
+
+class FlipLR(AugmentImage):
+    def __init__(self, p=.5, key_source='image', key_target=None):
+        super(FlipLR, self).__init__(key_source=key_source, key_target=key_target)
+
+        self.sequence = iaa.Fliplr(p=p)
+
+    def augment_img(self, image):
+        """
+        Apply augmentations
+        :param image: The image that needs to be augmented
+        :return: the augmented image
+        """
+        return self.sequence.augment_image(image)
+
+
+class RandomResizedCropFlip(AugmentImage):
+    """
+    Image augment class that only takes care of the correct padding and size
+    """
+
+    def __init__(self, key_source='image', key_target=None):
+        super(RandomResizedCropFlip, self).__init__(key_source=key_source, key_target=key_target)
+
+        self.sequence = iaa.Sequential([
+            iaa.size.Crop(percent=((0, .3), (0, .3), (0, .3), (0, .3))),
+            iaa.size.PadToSquare(pad_mode=imgaug.ALL),
+            iaa.Resize({"height": 128, "width": 128}),
+            iaa.Fliplr(p=.5)
         ])
 
     def augment_img(self, image):
